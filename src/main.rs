beeb8c079d62e245149761d6045a770243886b4b
Mario Sergio <marito@Marito-Macbook-Pro.hsd1.fl.comcast.net>
2024-07-29 23:35:13 -0400

    Run server UDPSocket and reponse Headers Struct

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+use std::net::UdpSocket;
+
+fn main() {
+    
+    let udp_socket = UdpSocket::bind("127.0.0.1:2053").expect("Failed to bind to address");
+    let mut buf = [0; 512];
+    
+    loop {
+        match udp_socket.recv_from(&mut buf) {
+            Ok((size, source)) => {
+                println!("Received {} bytes from {}", size, source);
+                let response = [0x04, 0xd2, 0x80, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+                udp_socket
+                    .send_to(&response, source)
+                    .expect("Failed to send response");
+            }
+            Err(e) => {
+                eprintln!("Error receiving data: {}", e);
+                break;
+            }
+        }
+    }
+}
